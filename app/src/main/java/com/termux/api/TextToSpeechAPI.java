0fd66d37e3f09a0fea280b41d43dfd15b9f69b1c
Fredrik Fornwall <fredrik@fornwall.net>
2016-04-20 11:24:02 +0200

    termux-tts-speak: Use STREAM_NOTIFICATION

--- app/src/main/java/com/termux/api/TextToSpeechAPI.java
+++ app/src/main/java/com/termux/api/TextToSpeechAPI.java
@@ -133,7 +133,7 @@ public class TextToSpeechAPI {
 
 						String utteranceId = "utterance_id";
 						Bundle params = new Bundle();
-						params.putInt(Engine.KEY_PARAM_STREAM, AudioManager.STREAM_ALARM);
+						params.putInt(Engine.KEY_PARAM_STREAM, AudioManager.STREAM_NOTIFICATION);
 						params.putString(Engine.KEY_PARAM_UTTERANCE_ID, utteranceId);
 
 						int submittedUtterances = 0;

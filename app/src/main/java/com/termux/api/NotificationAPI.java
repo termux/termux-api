899a7e353cbe9419e0e6a2cd24c19fad600f39fa
Fredrik Fornwall <fredrik@fornwall.net>
2016-12-04 04:24:29 +0100

    Add options to control notifictions

--- app/src/main/java/com/termux/api/NotificationAPI.java
+++ app/src/main/java/com/termux/api/NotificationAPI.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.Settings;
 
 import com.termux.api.util.ResultReturner;
 
@@ -18,26 +19,68 @@ public class NotificationAPI {
 
     static void onReceive(TermuxApiReceiver apiReceiver, final Context context, Intent intent) {
         String content = intent.getStringExtra("content");
+
         String notificationId = intent.getStringExtra("id");
-        if (notificationId == null) {
-            notificationId = UUID.randomUUID().toString();
+        if (notificationId == null) notificationId = UUID.randomUUID().toString();
+
+        String priorityExtra = intent.getStringExtra("priority");
+        if (priorityExtra == null) priorityExtra = "default";
+        int priority;
+        switch (priorityExtra) {
+            case "high":
+                priority = Notification.PRIORITY_HIGH;
+                break;
+            case "low":
+                priority = Notification.PRIORITY_LOW;
+                break;
+            case "max":
+                priority = Notification.PRIORITY_MAX;
+                break;
+            case "min":
+                priority = Notification.PRIORITY_MIN;
+                break;
+            default:
+                priority = Notification.PRIORITY_DEFAULT;
+                break;
         }
+
         String title = intent.getStringExtra("title");
 
-        String url = intent.getStringExtra("url");
-        PendingIntent pendingIntent = null;
-        if (url != null) {
-            Intent urlIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            pendingIntent = PendingIntent.getActivity(context, 0, urlIntent, 0);
-        }
+        String lightsArgbExtra = intent.getStringExtra("led-color");
+        int ledColor = (lightsArgbExtra == null) ? 0 : (Integer.parseInt(lightsArgbExtra, 16) | 0xff000000);
+        int ledOnMs = intent.getIntExtra("led-on", 800);
+        int ledOffMs = intent.getIntExtra("led-off", 800);
+
+        long[] vibratePattern = intent.getLongArrayExtra("vibrate");
+        boolean useSound = intent.getBooleanExtra("sound", false);
+
+        String urlExtra = intent.getStringExtra("url");
 
         Notification.Builder notification = new Notification.Builder(context);
-        notification.setSmallIcon(android.R.drawable.ic_popup_reminder);
+        notification.setSmallIcon(R.drawable.ic_event_note_black_24dp);
         notification.setColor(0xFF000000);
         notification.setContentTitle(title);
         notification.setContentText(content);
-        if (pendingIntent != null)
-            notification.setContentIntent(pendingIntent);
+        notification.setPriority(priority);
+        notification.setWhen(System.currentTimeMillis());
+
+        if (ledColor != 0) notification.setLights(ledColor, ledOnMs, ledOffMs);
+
+        if (vibratePattern != null) {
+            // Do not force the user to specify a delay first element, let it be 0.
+            long[] vibrateArg = new long[vibratePattern.length + 1];
+            System.arraycopy(vibratePattern, 0, vibrateArg, 1, vibratePattern.length);
+            notification.setVibrate(vibrateArg);
+        }
+
+        if (useSound) notification.setSound(Settings.System.DEFAULT_NOTIFICATION_URI);
+
+
+        if (urlExtra != null) {
+            Intent urlIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlExtra));
+            PendingIntent pi = PendingIntent.getActivity(context, 0, urlIntent, 0);
+            notification.setContentIntent(pi);
+        }
 
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(notificationId, 0, notification.build());

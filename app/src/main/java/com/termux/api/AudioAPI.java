18283e81afec9a9f5d27b0298ac7913e9ab4ce4f
its-pointless <bored@goat.si>
2018-01-30 08:28:07 +1100

    audio api

--- app/src/main/java/com/termux/api/AudioAPI.java
+++ app/src/main/java/com/termux/api/AudioAPI.java
@@ -17,10 +17,14 @@ public class AudioAPI {
 	    AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 	    String SampleRate = am.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
 	    String framesPerBuffer = am.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+	    String AudioUnprocessed = am.getProperty(AudioManager.PROPERTY_SUPPORT_AUDIO_SOURCE_UNPROCESSED);
+	    int volume_level= am.getStreamVolume(AudioManager.STREAM_MUSIC);
 	    public void writeJson(JsonWriter out) throws Exception {
 		out.beginObject(); 
 		out.name("PROPERTY_OUTPUT_SAMPLE_RATE").value(SampleRate);
 		out.name("PROPERTY_OUTPUT_FRAMES_PER_BUFFER").value(framesPerBuffer);
+		out.name("PROPERTY_SUPPORT_AUDIO_SOURCE_UNPROCESSED").value(AudioUnprocessed);
+		out.name("STREAM_MUSIC").value(volume_level);
 		out.endObject();
 		}
 	});

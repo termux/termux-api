a8f924a4a75285c7067611bfa12dd316377fc3c4
tomty89 <tom.ty89@gmail.com>
2018-02-02 14:28:30 +0800

    AudioAPI: get main code out of ResultJsonWriter

--- app/src/main/java/com/termux/api/AudioAPI.java
+++ app/src/main/java/com/termux/api/AudioAPI.java
@@ -9,30 +9,28 @@ import android.media.AudioTrack;
 import com.termux.api.util.ResultReturner;
 import com.termux.api.util.ResultReturner.ResultWriter;
 
-
-
 public class AudioAPI {
     static void onReceive(TermuxApiReceiver apiReceiver, final Context context, Intent intent) {
-	ResultReturner.returnData(apiReceiver, intent, new ResultReturner.ResultJsonWriter() {
-	    
-	    AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-	    String SampleRate = am.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
-	    String framesPerBuffer = am.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
-	    String AudioUnprocessed = am.getProperty(AudioManager.PROPERTY_SUPPORT_AUDIO_SOURCE_UNPROCESSED);
-	    int nativeoutput = AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC); 
-	    int volume_level= am.getStreamVolume(AudioManager.STREAM_MUSIC);
-	    int maxvolume_level = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-	    public void writeJson(JsonWriter out) throws Exception {
-		out.beginObject(); 
-		out.name("PROPERTY_OUTPUT_SAMPLE_RATE").value(SampleRate);
-		out.name("PROPERTY_OUTPUT_FRAMES_PER_BUFFER").value(framesPerBuffer);
-		out.name("PROPERTY_SUPPORT_AUDIO_SOURCE_UNPROCESSED").value(AudioUnprocessed);
-		out.name("STREAM_MUSIC_VOLUME").value(volume_level);
-		out.name("STREAM_MUSIC_MAXVOLUME").value(maxvolume_level);
-		out.name("CURRENT_NATIVE_OUTPUT_SAMPLERATE").value(nativeoutput);
-		out.endObject();
-		}
-	});
+        AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        String SampleRate = am.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+        String framesPerBuffer = am.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+        String AudioUnprocessed = am.getProperty(AudioManager.PROPERTY_SUPPORT_AUDIO_SOURCE_UNPROCESSED);
+        int nativeoutput = AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC);
+        int volume_level= am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int maxvolume_level = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        ResultReturner.returnData(apiReceiver, intent, new ResultReturner.ResultJsonWriter() {
+            public void writeJson(JsonWriter out) throws Exception {
+                out.beginObject();
+                out.name("PROPERTY_OUTPUT_SAMPLE_RATE").value(SampleRate);
+                out.name("PROPERTY_OUTPUT_FRAMES_PER_BUFFER").value(framesPerBuffer);
+                out.name("PROPERTY_SUPPORT_AUDIO_SOURCE_UNPROCESSED").value(AudioUnprocessed);
+                out.name("STREAM_MUSIC_VOLUME").value(volume_level);
+                out.name("STREAM_MUSIC_MAXVOLUME").value(maxvolume_level);
+                out.name("CURRENT_NATIVE_OUTPUT_SAMPLERATE").value(nativeoutput);
+                out.endObject();
+                }
+        });
     }
 }
-

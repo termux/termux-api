fa5268e6d4dd01153c645d3ddc8f0842c2bb0344
David Kramer <daveskypilot@gmail.com>
2018-06-18 13:00:38 -0600

    Add update reminder dialog / refactor versioning methods to TermuxApiHelper class

--- app/src/main/java/com/termux/api/util/TermuxApiVerifyActivity.java
+++ app/src/main/java/com/termux/api/util/TermuxApiVerifyActivity.java
@@ -8,6 +8,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -16,6 +17,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.view.Window;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.termux.api.R;
@@ -28,6 +30,12 @@ import java.io.File;
  */
 public class TermuxApiVerifyActivity extends Activity {
     private static final String TERMUX_API_WIKI_URL = "https://wiki.termux.com/wiki/Termux:API";
+    private static final String EXTRA_NOTIFY_TYPE = "notify_type";
+
+
+    private static final int EXTRA_NOTIFY_NOT_INSTALLED = R.layout.dialog_missing_tools;
+    private static final int EXTRA_NOTIFY_UPDATE = R.layout.dialog_tools_update;
+
     private static final int DIALOG_DELAY = 2500;
 
 
@@ -35,13 +43,24 @@ public class TermuxApiVerifyActivity extends Activity {
 
 
     public static void checkAndNotifyToolStatus(final Context context) {
-        if (!hasApiToolsInstalled()) {
+        boolean shouldNotifyUser = false;
+
+        final Intent notifyIntent = new Intent(context, TermuxApiVerifyActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        if (!TermuxApiHelper.hasToolsInstalled()) {
+            notifyIntent.putExtra(EXTRA_NOTIFY_TYPE, EXTRA_NOTIFY_NOT_INSTALLED);
+            shouldNotifyUser = true;
+        } else if (TermuxApiHelper.checkIfVersionChanged(context)) {
+            notifyIntent.putExtra(EXTRA_NOTIFY_TYPE, EXTRA_NOTIFY_UPDATE);
+            shouldNotifyUser = TermuxApiHelper.canRemindUser(context);
+        }
+
+        if (shouldNotifyUser) {
             new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    Intent installIntent = new Intent(context, TermuxApiVerifyActivity.class);
-                    installIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startActivity(installIntent);
+                    context.startActivity(notifyIntent);
                 }
             }, DIALOG_DELAY);
         }
@@ -56,7 +75,8 @@ public class TermuxApiVerifyActivity extends Activity {
     }
 
     protected void showDialog() {
-        View dialogView = View.inflate(this, R.layout.dialog_missing_tools, null);
+        int dialogType = getIntent().getIntExtra(EXTRA_NOTIFY_TYPE, 0);
+        View dialogView = View.inflate(this, dialogType, null);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         dialog = getDialog(dialogView);
@@ -74,7 +94,22 @@ public class TermuxApiVerifyActivity extends Activity {
 
         // install termux-api and use our notification api to display success
         String command = "pkg install termux-api && termux-notification --title 'Termux:API!' --content 'termux-api package installed successfully!'";
-        execTermuxCommand(this, command);
+        TermuxApiHelper.execTermuxCommand(this, command);
+        TermuxApiHelper.saveVersion(this);
+    }
+
+    /**
+     * Updates Termux packages
+     * @param view
+     */
+    public void updateButtonClicked(View view) {
+        dismiss();
+
+        Toast.makeText(this, "Updated packages", Toast.LENGTH_SHORT).show();
+
+        // update packages and use our notification api to display success
+        String command = "yes | pkg update && termux-notification --title 'Termux:API!' --content 'packages updated successfully!'";
+        TermuxApiHelper.execTermuxCommand(this, command);
     }
 
     /**
@@ -89,9 +124,19 @@ public class TermuxApiVerifyActivity extends Activity {
         startActivity(urlIntent);
     }
 
+    /**
+     * Toggle preference to remind user
+     * @param view
+     */
+    public void remindMeCheckBoxClicked(View view) {
+        CheckBox checkBox = (CheckBox)view;
+        // if box is NOT checked, we CAN remind the user
+        TermuxApiHelper.setCanRemindUser(this, !checkBox.isChecked());
+    }
+
     protected Dialog getDialog(View dialogView) {
         Dialog dialog = new AlertDialog.Builder(this)
-                .setTitle(getString(R.string.version, getInstalledTermuxAPIVersion(this)))
+                .setTitle(getString(R.string.version, TermuxApiHelper.getInstalledVersion(this)))
                 .setView(dialogView)
                 .setPositiveButton("Okay", null)
                 .setOnDismissListener(new DialogInterface.OnDismissListener() {
@@ -111,43 +156,91 @@ public class TermuxApiVerifyActivity extends Activity {
         }
     }
 
+
     /**
-     * Returns version number of our TermuxAPI
-     * @param context
-     * @return
+     * Helper class to manage installation
      */
-    protected static String getInstalledTermuxAPIVersion(Context context) {
-        String version = "Unknown";
-        try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            version = packageInfo.versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            TermuxApiLogger.error("Failed to obtain TermuxAPIVersion", e);
+    static final class TermuxApiHelper {
+        private static final String PREFERENCES = "termux_api_prefs";
+        private static final String KEY_API_VERSION = "api_version";
+        private static final String KEY_REMINDER = "reminder";
+
+        /**
+         * Check if installed version doesn't match our previously stored version
+         * @param context
+         * @return
+         */
+        static boolean checkIfVersionChanged(Context context) {
+            SharedPreferences preferences = context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+            final String installedApiVersion = getInstalledVersion(context);
+            return !preferences.getString(KEY_API_VERSION, "").equals(installedApiVersion);
         }
-        return version;
-    }
 
-
-    /**
-     * Executes specified command through Termux using a PendingIntent
-     * @param context
-     * @param command
-     */
-    protected static void execTermuxCommand(Context context, String command) {
-        PendingIntent pi = TermuxIntentHelper.createPendingIntent(context, command);
-        try {
-            pi.send();
-        } catch (PendingIntent.CanceledException e) {
-            TermuxApiLogger.error("execTermuxCommand error", e);
+        /**
+         * Checks to see if we can remind user
+         * @param context
+         * @return
+         */
+        static boolean canRemindUser(Context context) {
+            SharedPreferences preferences = context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+            return preferences.getBoolean(KEY_REMINDER, true);
         }
-    }
 
-    /**
-     * Checks to see if 'termux-api' pkg is installed
-     * @return
-     */
-    @SuppressLint("SdCardPath")
-    protected static boolean hasApiToolsInstalled() {
-        return new File("/data/data/com.termux/files/usr/libexec/termux-api").exists();
+        /**
+         * Returns version number of our TermuxAPI
+         * @param context
+         * @return
+         */
+        static String getInstalledVersion(Context context) {
+            String version = "Unknown";
+            try {
+                PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+                version = packageInfo.versionName;
+            } catch (PackageManager.NameNotFoundException e) {
+                TermuxApiLogger.error("Failed to obtain TermuxAPIVersion", e);
+            }
+            return version;
+        }
+
+        /**
+         * Executes specified command through Termux using a PendingIntent
+         * @param context
+         * @param command
+         */
+        static void execTermuxCommand(Context context, String command) {
+            PendingIntent pi = TermuxIntentHelper.createPendingIntent(context, command);
+            try {
+                pi.send();
+            } catch (PendingIntent.CanceledException e) {
+                TermuxApiLogger.error("execTermuxCommand error", e);
+            }
+        }
+
+        /**
+         * Checks to see if 'termux-api' pkg is installed
+         * @return
+         */
+        @SuppressLint("SdCardPath")
+        static boolean hasToolsInstalled() {
+            return new File("/data/data/com.termux/files/usr/libexec/termux-api").exists();
+        }
+
+        /**
+         * Saves installed version to preferences
+         * @param context
+         */
+        static void saveVersion(Context context) {
+            SharedPreferences preferences = context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(KEY_API_VERSION, getInstalledVersion(context));
+            editor.apply();
+        }
+
+        static void setCanRemindUser(Context context, boolean canRemind) {
+            SharedPreferences preferences = context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean(KEY_REMINDER, canRemind);
+            editor.apply();
+        }
     }
 }

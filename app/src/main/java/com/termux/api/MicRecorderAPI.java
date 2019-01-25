3bfb11fe0f3b4f1c5153c019ec7339498c46193c
tomty89 <tom.ty89@gmail.com>
2019-01-25 08:07:53 +0800

    MicRecoderAPI: a few enhancements (#248)
    * MicRecorderAPI: allow duration limit to be disabled with zero or negative
    
    * MicRecorderAPI: avoid unnecessary MediaRecorder parameter in RecorderCommandHandler.handle()
    
    * MicRecorderAPI: allow encoder and format to be set by user
    
    All supported encoders can be set with their names. Unless a format
    is set explicitly with its corresponding integer, a suitable format
    would be chosen automatically for the chosen encoder.
    
    * MicRecorderAPI: allow AudioSource to be set by user
    
    Just in case, so it can only be set with the source's corresponding
    integer.
    
    * MicRecorderAPI: allow bit rate, sampling rate and number of channels to be set by user
    
    * MicRecorderAPI: a couple of fixes
    
    Have year before month and day in the default file name for better
    sorting. Also fix the extension name in it for automatic format
    selection. As we want the file to have extension name by default,
    we have to pick a default format/encoder. AAC is chosen because it
    is probably the most compatible one among different platforms.
    
    * MicRecorderAPI: replace all getAbsoluteFile() with getAbsolutePath()
    
    None of them were used in places where it should be a File.
    
    * MicRecorderAPI: properly report unlimited MaxDuration
    
    * MicRecorderAPI: make sure the recorder is released when not used
    
    * MicRecorderAPI: remove aac_eld/he_aac/vorbis support
    
    They are at least problematic on my phone. They either fail to work
    or crash the API app. Sometimes the crash is even unrecoverable.

--- app/src/main/java/com/termux/api/MicRecorderAPI.java
+++ app/src/main/java/com/termux/api/MicRecorderAPI.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.os.IBinder;
+import android.util.ArrayMap;
+import android.util.SparseArray;
+import android.util.SparseIntArray;
 
 import com.termux.api.util.ResultReturner;
 import com.termux.api.util.TermuxApiLogger;
@@ -56,14 +59,16 @@ public class MicRecorderAPI {
         protected static File file;
 
 
-        public int onStartCommand(Intent intent, int flags, int startId) {
-            String command = intent.getAction();
-            MediaRecorder recorder = getMediaRecorder();
-            Context context = getApplicationContext();
+        public void onCreate() {
+            getMediaRecorder(this);
+        }
 
+        public int onStartCommand(Intent intent, int flags, int startId) {
             // get command handler and display result
+            String command = intent.getAction();
+            Context context = getApplicationContext();
             RecorderCommandHandler handler = getRecorderCommandHandler(command);
-            RecorderCommandResult result = handler.handle(recorder, context, intent);
+            RecorderCommandResult result = handler.handle(context, intent);
             postRecordCommandResult(context, intent, result);
 
             return Service.START_NOT_STICKY;
@@ -80,9 +85,11 @@ public class MicRecorderAPI {
                 default:
                     return new RecorderCommandHandler() {
                         @Override
-                        public RecorderCommandResult handle(MediaRecorder recorder, Context context, Intent intent) {
+                        public RecorderCommandResult handle(Context context, Intent intent) {
                             RecorderCommandResult result = new RecorderCommandResult();
                             result.error = "Unknown command: " + command;
+                            if (!isRecording)
+                                context.stopService(intent);
                             return result;
                         }
                     };
@@ -108,20 +115,13 @@ public class MicRecorderAPI {
         /**
          * Returns our MediaPlayer instance and ensures it has all the necessary callbacks
          */
-        protected MediaRecorder getMediaRecorder() {
-            if (mediaRecorder == null) {
-                mediaRecorder = new MediaRecorder();
-                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-                mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-                mediaRecorder.setOnErrorListener(this);
-                mediaRecorder.setOnInfoListener(this);
-            }
-            return mediaRecorder;
+        protected static void getMediaRecorder(MicRecorderService service) {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setOnErrorListener(service);
+            mediaRecorder.setOnInfoListener(service);
         }
 
         public void onDestroy() {
-            super.onDestroy();
             cleanupMediaRecorder();
             TermuxApiLogger.info("MicRecorderAPI MicRecorderService onDestroy()");
         }
@@ -130,11 +130,12 @@ public class MicRecorderAPI {
          * Releases MediaRecorder resources
          */
         protected static void cleanupMediaRecorder() {
-            if (mediaRecorder != null) {
+            if (isRecording) {
                 mediaRecorder.stop();
-                mediaRecorder.release();
-                mediaRecorder = null;
+                isRecording = false;
             }
+            mediaRecorder.reset();
+            mediaRecorder.release();
         }
 
         @Override
@@ -143,29 +144,26 @@ public class MicRecorderAPI {
         }
 
         @Override
-        public void onError(MediaRecorder mediaRecorder, int what, int extra) {
+        public void onError(MediaRecorder mr, int what, int extra) {
+            isRecording = false;
+            this.stopSelf();
             TermuxApiLogger.error("MicRecorderService onError() " + what);
         }
 
         @Override
-        public void onInfo(MediaRecorder mediaRecorder, int what, int extra) {
+        public void onInfo(MediaRecorder mr, int what, int extra) {
             switch (what) {
                 case MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED: // intentional fallthrough
                 case MEDIA_RECORDER_INFO_MAX_DURATION_REACHED:
-                    finishRecording();
+                    this.stopSelf();
             }
             TermuxApiLogger.info("MicRecorderService onInfo() " + what);
         }
 
-        protected static void finishRecording() {
-            isRecording = false;
-            cleanupMediaRecorder();
-        }
-
         protected static String getDefaultRecordingFilename() {
-            DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy_HH-mm-ss");
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
             Date date = new Date();
-            return Environment.getExternalStorageDirectory().getAbsolutePath() + "/TermuxAudioRecording_" + dateFormat.format(date) + ".3gp";
+            return Environment.getExternalStorageDirectory().getAbsolutePath() + "/TermuxAudioRecording_" + dateFormat.format(date);
         }
 
         protected static String getRecordingInfoJSONString() {
@@ -173,10 +171,8 @@ public class MicRecorderAPI {
             JSONObject info = new JSONObject();
             try {
                 info.put("isRecording", isRecording);
-
-                if (isRecording) {
-                    info.put("outputFile", file.getAbsoluteFile());
-                }
+                if (isRecording)
+                    info.put("outputFile", file.getAbsolutePath());
                 result = info.toString(2);
             } catch (JSONException e) {
                 TermuxApiLogger.error("infoHandler json error", e);
@@ -193,26 +189,63 @@ public class MicRecorderAPI {
 
         static RecorderCommandHandler infoHandler = new RecorderCommandHandler() {
             @Override
-            public RecorderCommandResult handle(MediaRecorder recorder, Context context, Intent intent) {
+            public RecorderCommandResult handle(Context context, Intent intent) {
                 RecorderCommandResult result = new RecorderCommandResult();
                 result.message = getRecordingInfoJSONString();
+                if (!isRecording)
+                    context.stopService(intent);
                 return result;
             }
         };
 
         static RecorderCommandHandler recordHandler = new RecorderCommandHandler() {
             @Override
-            public RecorderCommandResult handle(MediaRecorder recorder, Context context, Intent intent) {
+            public RecorderCommandResult handle(Context context, Intent intent) {
                 RecorderCommandResult result = new RecorderCommandResult();
 
-                String filename = intent.hasExtra("file") ? intent.getStringExtra("file") : getDefaultRecordingFilename();
-
                 int duration = intent.getIntExtra("limit", DEFAULT_RECORDING_LIMIT);
-                duration = duration < MIN_RECORDING_LIMIT ? MIN_RECORDING_LIMIT : duration;
+                // allow the duration limit to be disabled with zero or negative
+                if (duration > 0 && duration < MIN_RECORDING_LIMIT)
+                    duration = MIN_RECORDING_LIMIT;
+
+                String sencoder = intent.hasExtra("encoder") ? intent.getStringExtra("encoder") : "";
+                ArrayMap<String, Integer> encoder_map = new ArrayMap<>(3);
+                encoder_map.put("aac", MediaRecorder.AudioEncoder.AAC);
+                encoder_map.put("amr_nb", MediaRecorder.AudioEncoder.AMR_NB);
+                encoder_map.put("amr_wb", MediaRecorder.AudioEncoder.AMR_WB);
+
+                Integer encoder = encoder_map.get(sencoder.toLowerCase());
+                if (encoder == null)
+                    encoder = MediaRecorder.AudioEncoder.AAC;
+
+                int format = intent.getIntExtra("format", MediaRecorder.OutputFormat.DEFAULT);
+                if (format == MediaRecorder.OutputFormat.DEFAULT) {
+                    SparseIntArray format_map = new SparseIntArray(3);
+                    format_map.put(MediaRecorder.AudioEncoder.AAC,
+                                   MediaRecorder.OutputFormat.MPEG_4);
+                    format_map.put(MediaRecorder.AudioEncoder.AMR_NB,
+                                   MediaRecorder.OutputFormat.THREE_GPP);
+                    format_map.put(MediaRecorder.AudioEncoder.AMR_WB,
+                                   MediaRecorder.OutputFormat.THREE_GPP);
+                    format = format_map.get(encoder, MediaRecorder.OutputFormat.DEFAULT);
+                }
+
+                SparseArray<String> extension_map = new SparseArray<>(2);
+                extension_map.put(MediaRecorder.OutputFormat.MPEG_4, ".m4a");
+                extension_map.put(MediaRecorder.OutputFormat.THREE_GPP, ".3gp");
+                String extension = extension_map.get(format);
+
+                String filename = intent.hasExtra("file") ? intent.getStringExtra("file") : getDefaultRecordingFilename() + (extension != null ? extension : "");
+
+                int source = intent.getIntExtra("source", MediaRecorder.AudioSource.MIC);
+
+                int bitrate = intent.getIntExtra("bitrate", 0);
+                int srate = intent.getIntExtra("srate", 0);
+                int channels = intent.getIntExtra("channels", 0);
 
                 file = new File(filename);
 
-                TermuxApiLogger.info("MediaRecording file is: " + file.getAbsoluteFile());
+                TermuxApiLogger.info("MediaRecording file is: " + file.getAbsolutePath());
 
                 if (file.exists()) {
                     result.error = String.format("File: %s already exists! Please specify a different filename", file.getName());
@@ -221,13 +254,26 @@ public class MicRecorderAPI {
                         result.error = "Recording already in progress!";
                     } else {
                         try {
-                            recorder.setOutputFile(filename);
-                            recorder.setMaxDuration(duration);
+                            mediaRecorder.setAudioSource(source);
+                            mediaRecorder.setOutputFormat(format);
+                            mediaRecorder.setAudioEncoder(encoder);
+                            mediaRecorder.setOutputFile(filename);
+                            mediaRecorder.setMaxDuration(duration);
+                            if (bitrate > 0)
+                                mediaRecorder.setAudioEncodingBitRate(bitrate);
+                            if (srate > 0)
+                                mediaRecorder.setAudioSamplingRate(srate);
+                            if (channels > 0)
+                                mediaRecorder.setAudioChannels(channels);
                             mediaRecorder.prepare();
                             mediaRecorder.start();
                             isRecording = true;
                             result.message = String.format("Recording started: %s \nMax Duration: %s",
-                                    file.getAbsoluteFile(), MediaPlayerAPI.getTimeString(duration / 1000));
+                                                           file.getAbsolutePath(),
+                                                           duration <= 0 ?
+                                                           "unlimited" :
+                                                           MediaPlayerAPI.getTimeString(duration /
+                                                                                        1000));
 
                         } catch (IllegalStateException | IOException e) {
                             TermuxApiLogger.error("MediaRecorder error", e);
@@ -235,21 +281,23 @@ public class MicRecorderAPI {
                         }
                     }
                 }
+                if (!isRecording)
+                    context.stopService(intent);
                 return result;
             }
         };
 
         static RecorderCommandHandler quitHandler = new RecorderCommandHandler() {
             @Override
-            public RecorderCommandResult handle(MediaRecorder recorder, Context context, Intent intent) {
+            public RecorderCommandResult handle(Context context, Intent intent) {
                 RecorderCommandResult result = new RecorderCommandResult();
 
                 if (isRecording) {
-                    finishRecording();
-                    result.message = "Recording finished: " + file.getAbsoluteFile();
+                    result.message = "Recording finished: " + file.getAbsolutePath();
                 } else {
                     result.message = "No recording to stop";
                 }
+                context.stopService(intent);
                 return result;
             }
         };
@@ -259,7 +307,7 @@ public class MicRecorderAPI {
      * Interface for handling recorder commands
      */
     interface RecorderCommandHandler {
-        RecorderCommandResult handle(MediaRecorder recorder, final Context context, final Intent intent);
+        RecorderCommandResult handle(final Context context, final Intent intent);
     }
 
     /**

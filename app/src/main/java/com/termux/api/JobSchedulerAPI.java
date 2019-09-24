d75295d90237e84a6b01906abe412596238d350c
Daniil Gentili <daniil@daniil.it>
2019-09-24 17:45:28 +0200

    Fix fingerprint APIs on android 9 and switch to android X (#283)

--- app/src/main/java/com/termux/api/JobSchedulerAPI.java
+++ app/src/main/java/com/termux/api/JobSchedulerAPI.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.PersistableBundle;
-import android.support.annotation.RequiresApi;
+import androidx.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.util.Log;
 

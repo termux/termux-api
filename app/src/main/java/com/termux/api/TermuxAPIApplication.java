c4955a53b3a908b23756f1faae186179f594cddf
Henrik Grimler <henrik@grimler.se>
2021-12-05 17:09:31 +0100

    Define TermuxAPIApplication
    As part of adaption for the termux-shared library.

---
+++ app/src/main/java/com/termux/api/TermuxAPIApplication.java
@@ -0,0 +1,32 @@
+package com.termux.api;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.termux.shared.crash.TermuxCrashUtils;
+import com.termux.shared.logger.Logger;
+import com.termux.shared.settings.preferences.TermuxAPIAppSharedPreferences;
+
+
+public class TermuxAPIApplication extends Application {
+
+    public void onCreate() {
+        super.onCreate();
+
+        // Set crash handler for the app
+        TermuxCrashUtils.setCrashHandler(this);
+
+        // Set log config for the app
+        setLogLevel(getApplicationContext(), true);
+
+        Logger.logDebug("Starting Application");
+    }
+
+    public static void setLogLevel(Context context, boolean commitToFile) {
+        // Load the log level from shared preferences and set it to the {@link Logger.CURRENT_LOG_LEVEL}
+        TermuxAPIAppSharedPreferences preferences = TermuxAPIAppSharedPreferences.build(context);
+        if (preferences == null) return;
+        preferences.setLogLevel(null, preferences.getLogLevel(true), commitToFile);
+    }
+
+}

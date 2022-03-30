ba2836ba60083b54dcdb6a3a926777f3f0704829
agnostic-apollo <agnosticapollo@gmail.com>
2022-03-30 20:16:07 +0500

    Added: Request disable battery optimizations and grant draw over apps permission
    Previously, the `REQUEST_IGNORE_BATTERY_OPTIMIZATIONS` and `SYSTEM_ALERT_WINDOW` (draw over apps) permissions weren't requested by the `termux-api` app (only `termux-app`), so the options to disable/grant them wasn't shown in android settings. Moreover, `TermuxAPIActivity` have added warnings to request them if they have not been, in addition to vendor specific warnings, including link to https://dontkillmyapp.com.

--- app/src/main/java/com/termux/api/activities/TermuxAPIActivity.java
+++ app/src/main/java/com/termux/api/activities/TermuxAPIActivity.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.termux.api.util.ViewUtils;
 import com.termux.shared.activities.ReportActivity;
 import com.termux.shared.activity.ActivityUtils;
 import com.termux.shared.activity.media.AppCompatActivityUtils;
 import com.termux.shared.android.AndroidUtils;
+import com.termux.shared.android.PermissionUtils;
+import com.termux.shared.data.IntentUtils;
 import com.termux.shared.file.FileUtils;
 import com.termux.shared.logger.Logger;
 import com.termux.shared.models.ReportInfo;
@@ -23,6 +27,12 @@ import com.termux.shared.theme.NightMode;
 import com.termux.api.R;
 
 public class TermuxAPIActivity extends AppCompatActivity {
+
+    private TextView mBatteryOptimizationNotDisabledWarning;
+    private TextView mDisplayOverOtherAppsPermissionNotGrantedWarning;
+
+    private Button mDisableBatteryOptimization;
+    private Button mGrantDisplayOverOtherAppsPermission;
 
     private static final String LOG_TAG = "TermuxAPIActivity";
 
@@ -44,8 +54,23 @@ public class TermuxAPIActivity extends AppCompatActivity {
         pluginInfo.setText(getString(R.string.plugin_info, TermuxConstants.TERMUX_GITHUB_REPO_URL,
                 TermuxConstants.TERMUX_API_GITHUB_REPO_URL, TermuxConstants.TERMUX_API_APT_PACKAGE_NAME,
                 TermuxConstants.TERMUX_API_APT_GITHUB_REPO_URL));
+
+        mBatteryOptimizationNotDisabledWarning = findViewById(R.id.textview_battery_optimization_not_disabled_warning);
+        mDisableBatteryOptimization = findViewById(R.id.btn_disable_battery_optimizations);
+        mDisableBatteryOptimization.setOnClickListener(v -> requestDisableBatteryOptimizations());
+
+        mDisplayOverOtherAppsPermissionNotGrantedWarning = findViewById(R.id.textview_display_over_other_apps_not_granted_warning);
+        mGrantDisplayOverOtherAppsPermission = findViewById(R.id.btn_grant_display_over_other_apps_permission);
+        mGrantDisplayOverOtherAppsPermission.setOnClickListener(v -> requestDisplayOverOtherAppsPermission());
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        checkIfBatteryOptimizationNotDisabled();
+        checkIfDisplayOverOtherAppsPermissionNotGranted();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -91,6 +116,73 @@ public class TermuxAPIActivity extends AppCompatActivity {
             }
         }.start();
     }
+
+
+
+    private void checkIfBatteryOptimizationNotDisabled() {
+        if (mBatteryOptimizationNotDisabledWarning == null) return;
+
+        // If battery optimizations not disabled
+        if (!PermissionUtils.checkIfBatteryOptimizationsDisabled(this)) {
+            ViewUtils.setWarningTextViewAndButtonState(this, mBatteryOptimizationNotDisabledWarning,
+                    mDisableBatteryOptimization, true, getString(R.string.action_disable_battery_optimizations));
+        } else {
+            ViewUtils.setWarningTextViewAndButtonState(this, mBatteryOptimizationNotDisabledWarning,
+                    mDisableBatteryOptimization, false, getString(R.string.action_already_disabled));
+        }
+    }
+
+    private void requestDisableBatteryOptimizations() {
+        Logger.logDebug(LOG_TAG, "Requesting to disable battery optimizations");
+        PermissionUtils.requestDisableBatteryOptimizations(this, PermissionUtils.REQUEST_DISABLE_BATTERY_OPTIMIZATIONS);
+    }
+
+
+
+    private void checkIfDisplayOverOtherAppsPermissionNotGranted() {
+        if (mDisplayOverOtherAppsPermissionNotGrantedWarning == null) return;
+
+        // If display over other apps permission not granted
+        if (!PermissionUtils.checkDisplayOverOtherAppsPermission(this)) {
+            ViewUtils.setWarningTextViewAndButtonState(this, mDisplayOverOtherAppsPermissionNotGrantedWarning,
+                    mGrantDisplayOverOtherAppsPermission, true, getString(R.string.action_grant_display_over_other_apps_permission));
+        } else {
+            ViewUtils.setWarningTextViewAndButtonState(this, mDisplayOverOtherAppsPermissionNotGrantedWarning,
+                    mGrantDisplayOverOtherAppsPermission, false, getString(R.string.action_already_granted));
+        }
+    }
+
+    private void requestDisplayOverOtherAppsPermission() {
+        Logger.logDebug(LOG_TAG, "Requesting to grant display over other apps permission");
+        PermissionUtils.requestDisplayOverOtherAppsPermission(this, PermissionUtils.REQUEST_GRANT_DISPLAY_OVER_OTHER_APPS_PERMISSION);
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Logger.logVerbose(LOG_TAG, "onActivityResult: requestCode: " + requestCode + ", resultCode: "  + resultCode + ", data: "  + IntentUtils.getIntentString(data));
+
+        switch (requestCode) {
+            case PermissionUtils.REQUEST_DISABLE_BATTERY_OPTIMIZATIONS:
+                if(PermissionUtils.checkIfBatteryOptimizationsDisabled(this))
+                    Logger.logDebug(LOG_TAG, "Battery optimizations disabled by user on request.");
+                else
+                    Logger.logDebug(LOG_TAG, "Battery optimizations not disabled by user on request.");
+                break;
+            case PermissionUtils.REQUEST_GRANT_DISPLAY_OVER_OTHER_APPS_PERMISSION:
+                if(PermissionUtils.checkDisplayOverOtherAppsPermission(this))
+                    Logger.logDebug(LOG_TAG, "Display over other apps granted by user on request.");
+                else
+                    Logger.logDebug(LOG_TAG, "Display over other apps denied by user on request.");
+                break;
+            default:
+                Logger.logError(LOG_TAG, "Unknown request code \"" + requestCode + "\" passed to onRequestPermissionsResult");
+        }
+    }
+
+
 
     private void openSettings() {
         ActivityUtils.startActivity(this, new Intent().setClassName(TermuxConstants.TERMUX_PACKAGE_NAME, TermuxConstants.TERMUX_APP.TERMUX_SETTINGS_ACTIVITY_NAME));
---
+++ app/src/main/java/com/termux/api/util/ViewUtils.java
@@ -0,0 +1,33 @@
+package com.termux.api.util;
+
+import android.content.Context;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import com.termux.api.R;
+import com.termux.shared.theme.ThemeUtils;
+
+public class ViewUtils {
+
+    public static void setWarningTextViewAndButtonState(@NonNull Context context,
+                                                        @NonNull TextView textView, @NonNull Button button,
+                                                        boolean warningState, String text) {
+        if (warningState) {
+            textView.setTextColor(ContextCompat.getColor(context, R.color.red_error));
+            textView.setLinkTextColor(ContextCompat.getColor(context, R.color.red_error_link));
+            button.setEnabled(true);
+            button.setAlpha(1);
+        } else {
+            textView.setTextColor(ThemeUtils.getTextColorPrimary(context));
+            textView.setLinkTextColor(ThemeUtils.getTextColorLink(context));
+            button.setEnabled(false);
+            button.setAlpha(0.5f);
+        }
+
+        button.setText(text);
+    }
+
+}
